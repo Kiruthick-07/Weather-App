7f1ae7aef52cc270976452d439babad752ceebe0
Kiruthick R <152460421+Kiruthick-07@users.noreply.github.com>
2026-01-23 23:45:10 +0530

    App initial stage V1

--- app.py
+++ app.py
@@ -3,11 +3,33 @@ import requests
 
 app = Flask(__name__)
 
-@app.route("/"){
-    def home() : {
-        return render_template("index.html")
-    }
-}
+API_KEY="37159ac3bf9020ccfb53ad70a17732dd"
+
+@app.route("/", methods = ["GET" , "POST"])
+def home():
+    weather_data = None
+    error_data = None
+    if request.method == "POST":
+        city = request.form.get("city")
+        if city :
+            url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+            response = requests.get(url)
+            data = response.json()
+
+            if response.status_code == 200:
+                weather_data = {
+                    "city": data["name"],
+                    "temp": data["main"]["temp"],
+                    "humidity": data["main"]["humidity"],
+                    "wind": data["wind"]["speed"],
+                    "desc": data["weather"][0]["description"].title(),
+                    "icon": data["weather"][0]["icon"]
+                }
+            else:
+                error_data = "City not found! Please enter a valid city name."
+        else:
+            error_data = "Please enter a city name."
+    return render_template("index.html", weather=weather_data, error=error_data)
 
 if __name__ == "__main__":
     app.run(debug = True)
